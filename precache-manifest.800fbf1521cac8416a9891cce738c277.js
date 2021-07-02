1802acfc49aeb173b28c1ae860ff710ba5d1ec19
Sanm-ZH <zhxpvip7@gmail.com>
2021-07-03 00:02:24 +0800

    Updates

---
+++ precache-manifest.800fbf1521cac8416a9891cce738c277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa5f7cf4c7a6d0b04a3e",
+    "url": "/static/css/main.60350bc0.chunk.css"
+  },
+  {
+    "revision": "fa5f7cf4c7a6d0b04a3e",
+    "url": "/static/js/main.fa5f7cf4.chunk.js"
+  },
+  {
+    "revision": "10f204d7d2b201706a34",
+    "url": "/static/js/1.10f204d7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9731420e7e298ca1c7d3306c9322b174",
+    "url": "/index.html"
+  }
+];
