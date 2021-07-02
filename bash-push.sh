649669800321d7bd339de5fbe95cbf29bce53468
Sanm-ZH <zhxpvip7@gmail.com>
2021-07-02 23:56:50 +0800

    Added bash-push.sh

---
+++ bash-push.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+branch=$(git symbolic-ref --short -q HEAD)
+
+git push origin $branch:$branch
+echo -e '\033[32m 已推送至sanm-zh.github.io \n'
+
+git remote set-url origin git@gitee.com:sanm-zh/react-demo.git
+git push origin $branch:$branch
+echo -e '\033[32m 已推送至sanm-zh.gitee.io \n'
+
+git remote set-url origin git@github.com:Sanm-ZH/react-demo.git
