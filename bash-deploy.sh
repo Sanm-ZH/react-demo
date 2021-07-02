d12aeb9ed35bc545a2902e50e02ec467bdb875b6
Sanm-ZH <zhxpvip7@gmail.com>
2021-07-03 00:01:44 +0800

    Added deploy.sh

---
+++ bash-deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+branch=$(git symbolic-ref --short -q HEAD)
+
+pnpm build
+
+git push origin $branch:$branch
+pnpx gh-pages -d build
+echo -e '\033[32m 已部署成功至sanm-zh.github.io \n'
+
+git remote set-url origin git@gitee.com:sanm-zh/react-demo.git
+git push origin $branch:$branch
+pnpx gh-pages -d docs
+echo -e '\033[32m 已部署成功至sanm-zh.gitee.io'
+echo -e '\033[32m 如果不是gitee pro用户请手动更新gitee pages服务 \n'
+
+git remote set-url origin git@github.com:Sanm-ZH/react-demo.git
