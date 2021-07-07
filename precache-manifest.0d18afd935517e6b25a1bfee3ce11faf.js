bd5895a57d449be20861fd9feda19aeb855e11e6
Sanm-ZH <zhxpvip7@gmail.com>
2021-07-07 12:40:51 +0800

    Updates

---
+++ precache-manifest.0d18afd935517e6b25a1bfee3ce11faf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ba750aa1d17cdaf7246",
+    "url": "./static/css/main.16cc74f4.chunk.css"
+  },
+  {
+    "revision": "9ba750aa1d17cdaf7246",
+    "url": "./static/js/main.9ba750aa.chunk.js"
+  },
+  {
+    "revision": "10f204d7d2b201706a34",
+    "url": "./static/js/1.10f204d7.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f7df311c8daab640c8723c02145c2295",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d784e45623a504ee48d291c6598aba98.js"
+  "./precache-manifest.0d18afd935517e6b25a1bfee3ce11faf.js"
 );
 
 workbox.clientsClaim();
